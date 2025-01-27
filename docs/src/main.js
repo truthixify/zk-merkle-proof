58e67dfd263dbd2ba17ed746c1589c69127083e5
truthixify <truthixify@gmail.com>
2025-01-27 03:41:40 +0100

    added dist for web build

--- docs/src/main.js
+++ docs/src/main.js
@@ -93,13 +93,3 @@ async function main() {
 }
 
 main()
-  .then(() => {})
-  .catch((err) => {
-    Toastify({
-      text: "This is a toast",
-      className: "info",
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-    }).showToast();
-  });
